23b400f6c2b48f1f6ae7d8b818f00bededd96192
Pete M <petem801@gmail.com>
2024-09-10 23:42:27 -0400

    HW3 Update

--- features/steps/Target_Cart_Steps.py
+++ features/steps/Target_Cart_Steps.py
@@ -36,3 +36,19 @@ def verify_results(context):
         assert expected_result in actual_result, f'Expected {expected_result}, got actual {actual_result}'
 
 
+@when('Click Sign In')
+def click_sign_in(context):
+        context.driver.find_element(By.CSS_SELECTOR, "[class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']").click()
+        sleep(2)  # wait for search results page to load
+
+@when('Click Sign In From Side Menu')
+def click_signin_nav(context):
+        context.driver.find_element(By. CSS_SELECTOR, "[class='sc-859e7637-0 hHZPQy']").click()
+        sleep(2)  # wait for search results page to load
+
+@then('Verify Sign In form opened')
+def verify_results(context):
+        actual_result = context.driver.find_element(By. CSS_SELECTOR, ".WObnm").text
+        expected_result = 'Sign into your Target account'
+        assert expected_result in actual_result, f'Expected {expected_result}, got actual {actual_result}'
+
--- features/tests/Target_Tests_Cart.feature
+++ features/tests/Target_Tests_Cart.feature
@@ -10,6 +10,11 @@ Feature: Tests for Target Search functionality
     When Click on Cart icon
     Then Verify “Your cart is empty” message is shown
 
+  Scenario: User can select sign in
+    Given Open target main page
+    When Click Sign In
+    When Click Sign In From Side Menu
+    Then Verify Sign In form opened
 #  Scenario: User can see Cart Empty message
 #    Given Open target main page
 #    When Click on cart icon
